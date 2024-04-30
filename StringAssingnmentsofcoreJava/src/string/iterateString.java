93c674d70b0916de68e538a6427e97d24b755390
Akash Kumar <kumaraakash82334@gmail.com>
2024-04-30 13:50:57 +0530

    iterate over character in String

---
+++ StringAssingnmentsofcoreJava/src/string/iterateString.java
@@ -0,0 +1,12 @@
+package string;
+
+public class iterateString {
+public static void main(String[] args) {
+	String str="Hello! Akash";
+	System.out.println("Original String:  "+str);
+	for(int i=0;i<str.length();i++) {
+		System.out.println(str.charAt(i));
+	}
+	
+}
+}
