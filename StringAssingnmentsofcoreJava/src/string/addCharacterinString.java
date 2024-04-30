50159802fe6141a23799bb685ac79407d734f8d8
Akash Kumar <kumaraakash82334@gmail.com>
2024-04-30 13:32:53 +0530

    add character in a string

---
+++ StringAssingnmentsofcoreJava/src/string/addCharacterinString.java
@@ -0,0 +1,11 @@
+package string;
+
+public class addCharacterinString {
+public static void main(String[] args) {
+	String str="I am Akash ";
+	char a='!';
+	str+=a;
+	System.out.println(str);
+}
+
+}
