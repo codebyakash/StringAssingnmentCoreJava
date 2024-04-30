b619e9e41abb99120da9218c227d9eac0bdb98a3
Akash Kumar <kumaraakash82334@gmail.com>
2024-04-30 13:05:20 +0530

    new line in a string

---
+++ StringAssingnmentsofcoreJava/src/string/newLineinString.java
@@ -0,0 +1,9 @@
+package string;
+
+public class newLineinString {
+public static void main(String[] args) {
+	// we use \n for new line
+	String str="1. Akash\n2. Vikas";
+	System.out.println(str);
+}
+}
