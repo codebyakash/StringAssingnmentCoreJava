cfd626d9d598a063467b1fc59433963cd4bd227b
Akash Kumar <kumaraakash82334@gmail.com>
2024-05-01 20:40:34 +0530

    find unicode of specific character

---
+++ StringAssingnmentsofcoreJava/src/string/unicodePoint.java
@@ -0,0 +1,8 @@
+package string;
+
+public class unicodePoint {
+		public static void main(String[] args) {
+			String str = "Akashkumar";
+				System.out.println("unicode of you charater:"+(int)str.charAt(5));	
+			}
+		}
