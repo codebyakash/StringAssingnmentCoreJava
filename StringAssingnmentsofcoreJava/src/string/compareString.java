577dc1223f259bb6ffa586a73250b459f498f534
Akash Kumar <kumaraakash82334@gmail.com>
2024-05-01 20:46:22 +0530

    Compare Two string

---
+++ StringAssingnmentsofcoreJava/src/string/compareString.java
@@ -0,0 +1,15 @@
+package string;
+
+public class compareString {
+		public static void main(String[] args) {
+			String str ="akashkumar";
+			String str2="akashkumar";
+					if(str.equals(str2)) {
+						System.out.println("It is Compare String");
+					}
+						else
+							System.out.println("It is not Compare String");
+					
+				}
+	}
+
