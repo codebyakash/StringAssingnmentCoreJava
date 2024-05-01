fcdf824100e0957e57905527e1e3e101c10a4273
Akash Kumar <kumaraakash82334@gmail.com>
2024-05-01 14:02:05 +0530

    don'reverse special character

--- StringAssingnmentsofcoreJava/src/string/ReversePairsOfCharacter.java
+++ StringAssingnmentsofcoreJava/src/string/ReversePairsOfCharacter.java
@@ -5,11 +5,12 @@ public static void main(String[] args) {
 	String str = "Hello, world!";
     char[] ch= str.toCharArray();
     for (int i = 0; i < ch.length - 1; i+=2) {
+    	if(ch[i]>=65&&ch[i+1]<=122) {
         char temp = ch[i];
         ch[i] = ch[i + 1];
         ch[i + 1] = temp;
     }
-    
+    }
     System.out.print("Original string: " + str + "\nString after swapping pairs: ");
     System.out.println(ch);
 	}
